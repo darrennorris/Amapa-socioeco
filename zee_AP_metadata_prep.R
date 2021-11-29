6ae1e6d743219f88af377036ccebab3cd205d5a1
Darren Norris <doon75@hotmail.com>
2021-11-29 15:55:27 -0300

    prepare function files

---
+++ zee_AP_metadata_prep.R
@@ -0,0 +1,3 @@
+# zee_AP_metadata_prep. Metadata xml conforme ISO FGDC.
+# packages
+library(geodata)
---
+++ zee_AP_muni_visual.R
@@ -0,0 +1,3 @@
+
+#Build a little app
+#https://www.r-craft.org/r-news/interactive-visualization-of-geospatial-data-with-r-shiny/
