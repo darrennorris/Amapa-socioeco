1b942ce7bec27e3343c48b460b97e5bda5220176
Darren Norris <doon75@hotmail.com>
2021-12-05 09:45:04 -0300

    update data

--- zee_AP_cefm.R
+++ zee_AP_cefm.R
@@ -10,7 +10,7 @@ library(sf)
 library(viridis)
 library(gridExtra)
 
-dfmin <- read_excel("dados//ANM_CEFM.xlsx", sheet = "CFEM_municipios_mensal", 
+dfmin <- read_excel("dados//ANM_CFEM.xlsx", sheet = "CFEM_municipios_mensal", 
                     .name_repair = "universal") 
 unique(dfmin$SUBS)
 dfmin %>% mutate(SUBS_simples = 
@@ -34,11 +34,11 @@ unique(dfmin$SUBS_simples)
 # OURO (72.4, 58 Milhoes) e CAULIM (22.5, 18 Milhoes) = 94.9% (76 Milhoes)
 dfmin %>% filter(ano_ref %in% c(2021, 2020, 2019, 2018, 2017)) %>% 
   group_by(SUBS_simples) %>% 
-  summarise(total_cefm_2017_2021 = sum(Total_ano, na.rm=TRUE)) %>% 
-  ungroup() %>% arrange(desc(total_cefm_2017_2021)) %>% 
-  mutate(csum_cefm = cumsum(total_cefm_2017_2021)) %>% 
-  mutate(per_cefm = (total_cefm_2017_2021 / max(csum_cefm)) *100, 
-         cper_cefm = (csum_cefm / max(csum_cefm)) *100)
+  summarise(total_cfem_2017_2021 = sum(Total_ano, na.rm=TRUE)) %>% 
+  ungroup() %>% arrange(desc(total_cfem_2017_2021)) %>% 
+  mutate(csum_cfem = cumsum(total_cfem_2017_2021)) %>% 
+  mutate(per_cfem = (total_cfem_2017_2021 / max(csum_cfem)) *100, 
+         cper_cfem = (csum_cfem / max(csum_cfem)) *100)
 
 dfmin %>% mutate(SUBS_main = 
                    case_when(!(SUBS_simples %in% c( "OURO", "CAULIM", "FERRO", "CROMO", "GRANITO")) ~"outros", 
@@ -49,11 +49,11 @@ dfmin %>% mutate(SUBS_main =
 # VITÓRIA DO JARI (22.2, 18 Milhoes) = 90.2% , 72.3 Milhoes
 dfmin %>% filter(ano_ref %in% c(2021, 2020, 2019, 2018, 2017)) %>% 
   group_by(municipio) %>% 
-  summarise(total_cefm_2017_2021 = sum(Total_ano, na.rm=TRUE)) %>% 
-  ungroup() %>% arrange(desc(total_cefm_2017_2021)) %>% 
-  mutate(csum_cefm = cumsum(total_cefm_2017_2021)) %>% 
-  mutate(per_cefm = (total_cefm_2017_2021 / max(csum_cefm)) *100, 
-         cper_cefm = (csum_cefm / max(csum_cefm)) *100)
+  summarise(total_cfem_2017_2021 = sum(Total_ano, na.rm=TRUE)) %>% 
+  ungroup() %>% arrange(desc(total_cfem_2017_2021)) %>% 
+  mutate(csum_cfem = cumsum(total_cfem_2017_2021)) %>% 
+  mutate(per_cfem = (total_cfem_2017_2021 / max(csum_cfem)) *100, 
+         cper_cfem = (csum_cfem / max(csum_cfem)) *100)
 
 #From 2004 - 2021 median of 12 million per year (2.5 to 22.7 million)
 dfmin %>% 
@@ -91,20 +91,22 @@ dfmin %>%
   summarise(count_ano = length(unique(ano_ref)),
             first_ano = min(ano_ref), 
             last_ano = max(ano_ref),
-              mean_cefm = mean(tot_ano, na.rm=TRUE)) %>% 
+              mean_cfem = mean(tot_ano, na.rm=TRUE)) %>% 
   left_join(
 dfmin %>% 
   group_by(ano,ufn, codmun7, municipio) %>% 
-  summarise(total_cefm = sum(Total_ano, na.rm=TRUE)) %>% 
+  summarise(total_cfem = sum(Total_ano, na.rm=TRUE)) %>% 
   ungroup() %>% 
   group_by(ano) %>% 
-  arrange(ano, desc(total_cefm)) %>% 
-  mutate(csum_cefm = cumsum(total_cefm)) %>% 
-  mutate(per_cefm = (total_cefm / max(csum_cefm)) *100, 
-         cper_cefm = (csum_cefm / max(csum_cefm)) *100, 
+  arrange(ano, desc(total_cfem)) %>% 
+  mutate(csum_cfem = cumsum(total_cfem)) %>% 
+  mutate(per_cfem = (total_cfem / max(csum_cfem)) *100, 
+         cper_cfem = (csum_cfem / max(csum_cfem)) *100, 
          codmun7 = as.character(codmun7))
-) -> dfcefm
+) -> dfcfem
 
+#Export
+write.csv(dfcfem, "dados//cfem.csv", row.names = FALSE)
 #Poligonos municipios Amapa
 sf::st_read("vector//IBGE_Amazonia_Legal.GPKG", layer = "Mun_Amazonia_Legal_2020") %>% 
   filter(NM_UF == "Amapá") -> sf_ap_muni
@@ -114,16 +116,12 @@ sf_ap <- st_sf(data.frame(CD_UF="16", geom=sf_ap))
 #Lines
 sf_ap_muni_line <- st_cast(sf_ap_muni, "MULTILINESTRING")
 
-sf_ap_muni %>% right_join(dfcefm, by = c("CD_MUN" = "codmun7")) -> sf_cefm
+sf_ap_muni %>% right_join(dfcfem, by = c("CD_MUN" = "codmun7")) -> sf_cfem
 
-sf_cefm %>% 
-  mutate(per_cefm = ifelse(per_cefm ==0,NA, per_cefm)) %>%
-  mutate(per_cefm_log = log1p(per_cefm)) %>% pull(per_cefm_log)
-
-sf_cefm %>% 
-  mutate(per_cefm = ifelse(per_cefm ==0,NA, per_cefm)) %>%
+sf_cfem %>% 
+  mutate(per_cfem = ifelse(per_cfem ==0,NA, per_cfem)) %>%
   ggplot() + 
-  geom_sf(aes(fill = per_cefm)) + 
+  geom_sf(aes(fill = per_cfem)) + 
   scale_x_continuous(breaks = seq(-55, -49.8, by = 2)) +
   facet_wrap(~ano) + 
   theme_bw() + 
